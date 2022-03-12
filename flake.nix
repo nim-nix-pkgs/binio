4eed8329c4e0e68785f47bb51f9f524381f09744
hugosenari <hugosenari@gmail.com>
2022-03-12 00:47:54 -0300

    chore: re index binio

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Package for packing and unpacking byte data'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."binio-master".url = "path:./master";
+  inputs."binio-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."binio-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Package for packing and unpacking byte data'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-binio-master.flake = false;
+  inputs.src-binio-master.owner = "Riderfighter";
+  inputs.src-binio-master.ref   = "refs/heads/master";
+  inputs.src-binio-master.repo  = "binio";
+  inputs.src-binio-master.type  = "github";
+  
+  inputs."struct".dir   = "nimpkgs/s/struct";
+  inputs."struct".owner = "riinr";
+  inputs."struct".ref   = "flake-pinning";
+  inputs."struct".repo  = "flake-nimble";
+  inputs."struct".type  = "github";
+  inputs."struct".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."struct".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-binio-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-binio-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
