7e1c569fa17000bcc48c0b70886722cb391f5486
hugosenari <hugosenari@gmail.com>
2022-03-27 21:48:12 -0300

    chore: re index binio

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-binio-master.ref   = "refs/heads/master";
   inputs.src-binio-master.owner = "Riderfighter";
   inputs.src-binio-master.repo  = "binio";
-  inputs.src-binio-master.dir   = "";
   inputs.src-binio-master.type  = "github";
   
   inputs."struct".owner = "nim-nix-pkgs";
